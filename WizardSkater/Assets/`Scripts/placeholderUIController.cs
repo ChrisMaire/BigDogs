749411e3007dfa749f6a29f9c32037b41385fbea
Chasterson <philroycrafts@gmail.com>
2015-11-30 23:32:48 -0500

    Finally got a bad UI made, was thinking I would fill out those score and time scripts somebody threw into the GIT and get a bad main menu made tommorrow.

---
+++ WizardSkater/Assets/`Scripts/placeholderUIController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class placeholderUIController : MonoBehaviour {
+
+	public Text time;
+	public Text Score;
+	public Slider MANA;
+
+
+	// Use this for initialization
+	void Start () {
+
+	 
+	   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
