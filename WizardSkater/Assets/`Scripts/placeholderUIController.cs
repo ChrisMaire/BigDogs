dfe69b3732ed0fb8b55335e32f5f95c5519084cc
Chasterson <philroycrafts@gmail.com>
2015-12-06 23:43:15 -0500

    UI update - Working Speed Bar - Timer

--- WizardSkater/Assets/`Scripts/placeholderUIController.cs
+++ WizardSkater/Assets/`Scripts/placeholderUIController.cs
@@ -4,35 +4,61 @@ using System.Collections;
 
 public class placeholderUIController : MonoBehaviour {
 
-	public Text time;
+	public Text Timer;
 	public Text Score;
 	public Slider Speed;
-	public Component Player;
-	GameObject Wizard;
+	public Slider Action;
+	private GameObject m_player;
+	private bool active;
 
 	float maxspeed;
 	float speed;
+	float valuehold;
 
 	// Use this for initialization
 	void Start () {
 
-	 
+		bool active = true;
 	   
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-		//is wizard?
-		if (GameObject.Find ("Wizard"))
-		{	
-			Wizard = GameObject.Find ("Wizard");
+	private void LateUpdate()
+	{
+		if (m_player == null) {
+			m_player = SystemsManager.m_Player.gameObject;
+			if (m_player == null) {
+				Debug.Log ("where is pretty baby???");
+			}
+		}
+		
+		if ((SystemsManager.m_Game.getState () == Game.GameState.Testing || SystemsManager.m_Game.getState () == Game.GameState.Gameplay) &&
+			m_player != null) {
+			speed = m_player.GetComponent<Player> ().m_moveSpeedCurrent;
+			maxspeed = m_player.GetComponent<Player> ().m_speedTurbo + m_player.GetComponent<Player> ().m_moveSpeed3;
 
-			Player = Wizard.GetComponent<Player> ();
+			Speed.value = speed / maxspeed;
 
-			//handles speed meter
-			 
+			Timer.text = Time.time.ToString();
 
+			Score.text = "00000";
 		}
 	}
+			/*	if(!m_player.GetComponent<Player>().m_moveChangeReady){
+								ActionBar();
+			}
+		} */
+	
+
+	/*
+	private void ActionBar()
+	{
+	
+		if (active) { valuehold = 1;
+			Action.value = 1;
+		}
+		else {
+			Action.value = valuehold - .1f;
+		}
+
+	}*/ 
 }
