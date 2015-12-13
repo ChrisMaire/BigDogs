e2097a9aa20ebd9bc6a1901badafa4925e5dab6d
Christopher Maire <locura.madness@gmail.com>
2015-12-13 14:30:30 -0500

    Added a small basic particle to the Wizard that adjusts emission rate based on velocity.

---
+++ WizardSkater/Assets/`Scripts/SkatingParticle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkatingParticle : MonoBehaviour {
+    Rigidbody playerBody;
+    ParticleSystem particle;
+    ParticleSystem.EmissionModule emission;
+    ParticleSystem.MinMaxCurve curve;
+
+    float startRate;
+
+	void Start () {
+        particle = GetComponent<ParticleSystem>();
+        emission = particle.emission;
+        playerBody = FindObjectOfType<Player>().GetComponent<Rigidbody>();
+        curve = particle.emission.rate;
+        startRate = curve.constantMax;
+	}
+	
+	void Update () {
+        float rate = startRate * playerBody.velocity.magnitude;
+        Debug.Log(rate);
+        curve.constantMin = rate;
+        curve.constantMax = rate;
+        emission.rate = curve;
+	}
+}
